a5a16f80ebe8f0a9b0caea399b0401c4f80fb48b
david_cohn <david.cohn.1234@gmail.com>
2023-01-08 16:13:12 +0200

    added yield_tutorial

---
+++ yield_tutorial/script_yield_tutorial.py
@@ -0,0 +1,21 @@
+
+def csv_reader(file_name):
+    file = open(file_name)
+    result = file.read().split("\n")
+    return result
+
+def csv_reader_using_yield(file_name):
+    for row in open(file_name, "r"):
+        yield row
+
+def main():
+    csv_gen = csv_reader("david.csv")
+    #csv_gen = csv_reader_using_yield("david.csv")
+    row_count = 0
+
+    for row in csv_gen:
+        row_count += 1
+
+    print(f"Row count is {row_count}")
+
+main()
