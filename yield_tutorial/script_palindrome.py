63e03ede615ef3fdd46109e35ce2ecc4af5d3314
david <david.cohn.1234@gmail.com>
2023-01-10 00:59:29 +0200

    updated

--- yield_tutorial/script_palindrome.py
+++ yield_tutorial/script_palindrome.py
@@ -16,21 +16,27 @@ def is_palindrome(num):
 
 def infinite_palindromes():
     num = 0
+    print('infinite_palindromes: before while True')
     while True:
+        #print('inside function')
         if is_palindrome(num):
-            i = (yield num)
-            print(f'{i}: inside infinite_palindromes')
-            print()
+            print(f'infinite_palindromes: num = {num}')
+            i = (yield num+1)
+            print(f'infinite_palindromes: i={i}')
+            #print(f'num={num}: inside infinite_palindromes')
             if i is not None:
                 num = i
         num += 1
 
 def main():
     pal_gen = infinite_palindromes()
-    for i in pal_gen:
-        print(i)
-        digits = len(str(i))
-        #pal_gen.send(10 ** (digits))
+    for j in range(5):
+        print(f'main: j={j}, before next(pal_gen)')
+        k = next(pal_gen)
+        print(f'main: k={k}, after next(pal_gen)')
+        digits = len(str(k))
+        pal_gen.send(10 ** (digits))
+        print()
 
     # for i in range(200):
     #     if is_palindrome(i):
