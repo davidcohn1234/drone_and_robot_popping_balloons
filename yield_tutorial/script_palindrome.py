06d599f12a8fe882a0e2451520df26622518c8d7
david_cohn <david.cohn.1234@gmail.com>
2023-01-09 16:17:37 +0200

    updated

--- yield_tutorial/script_palindrome.py
+++ yield_tutorial/script_palindrome.py
@@ -19,6 +19,8 @@ def infinite_palindromes():
     while True:
         if is_palindrome(num):
             i = (yield num)
+            print(f'{i}: inside infinite_palindromes')
+            print()
             if i is not None:
                 num = i
         num += 1
@@ -28,7 +30,7 @@ def main():
     for i in pal_gen:
         print(i)
         digits = len(str(i))
-        pal_gen.send(10 ** (digits))
+        #pal_gen.send(10 ** (digits))
 
     # for i in range(200):
     #     if is_palindrome(i):
